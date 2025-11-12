04c89a6e0d06e6a9b5b8f9914cabadb745d76cf0
Shivam00609 <shivammaurya6025@gmail.com>
2025-11-12 11:09:09 +0530

    Add files via upload

---
+++ function in python for library.py
@@ -0,0 +1,37 @@
+
+'''print("welcome User! how can i help you ")
+def User_input(continue,exit):'''
+print("Welcome")
+def run():
+    print("press 1 For continue")
+    print("press 2 For exit")
+    a = int(input("Enter any no. as per given above : "))
+    if a == 1:
+            book()
+    elif a == 2:
+            print("Thanks for visit")
+    else:
+            print("Invaild input given")
+run()
+def book():
+    print("press 1 for book1: To Kill a Mockingbird by Harper Lee")
+    print("press 2 for book2: 1984 by George Orwell")
+    print("press 3 for book3: The Alchemist by Paulo Coelho")
+    print("press 4 for book4: The Great Gatsby by F. Scott Fitzgerald")
+    print("press 5 for book5: Harry Potter and the Sorcerer’s Stone by J.K. Rowling")
+
+    b = int(input("Enter a book for as shown above:"))
+    if b == 1:
+        print("To Kill a Mockingbird by Harper Lee -> \n-------- Description -------- \n A timeless classic exploring racial injustice and moral growth in the Deep South. Told through the eyes of young Scout Finch, it teaches empathy, courage, and integrity.")
+    elif b == 2:
+        print("1984 by George Orwell -> \n-------- Description -------- \n A dystopian novel about a totalitarian regime that controls truth, history, and thought. It’s a powerful warning about surveillance, propaganda, and loss of freedom.")
+    elif b == 3:
+        print("The Alchemist by Paulo Coelho -> \n-------- Description -------- \n A philosophical tale about following one’s dreams and listening to the heart. It follows Santiago, a shepherd, on his journey to find his personal legend")
+    elif b == 4:
+        print("The Great Gatsby by F. Scott Fitzgerald -> \n-------- Description -------- \n Set in the roaring 1920s, it depicts love, wealth, and the illusion of the American Dream. Jay Gatsby’s tragic pursuit of Daisy Buchanan reflects the emptiness of materialism.")
+    elif b == 5:
+        print("Harry Potter and the Sorcerer’s Stone by J.K. Rowling -> \n-------- Description -------- \n The first book in the magical Harry Potter series. It introduces a boy wizard discovering friendship, courage, and destiny at Hogwarts. ")
+    else:
+        print("Invalid Input \n ---------- No Book Found ----------")
+        
+book()
